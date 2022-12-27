dd2d99be32a054231edaee1ab45f393d419fe022
Farhan <faarhan7@gmail.com>
2022-12-27 13:34:54 +0700

    mentor new migration and relationship

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;   
@@ -11,4 +12,10 @@ class Mentor extends Authenticatable
     use HasFactory;
     
     protected $guard = 'mentor';
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+        
+    }
 }
