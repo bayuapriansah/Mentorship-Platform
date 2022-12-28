13b839ec8ddd45dd116f6d8b3ef524cc5a6c7da0
Farhan <faarhan7@gmail.com>
2022-12-28 15:09:37 +0700

    admin can manage company

--- resources/views/dashboard/companies/index.blade.php
+++ resources/views/dashboard/companies/index.blade.php
@@ -4,19 +4,22 @@
 
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Partner</h1>
-      {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Add Students</a> --}}
+      <h1 class="h3 mb-0 text-gray-800">Companies</h1>
+      <a href="{{route('dashboard.companies.create')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fa-regular fa-plus"></i> Add Companies</a>
   </div>
 
   <!-- Content Row -->
   <div class="row">
-    <div class="col">
+    <div class="col card p-4">
+      @include('flash-message')
       <table id="myTable" class="display responsive w-100" style="width: 100%">
         <thead>
           <tr>
             <th>No</th>
             <th>Company Name</th>
             <th>Email</th>
+            <th>Address</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -26,6 +29,17 @@
             <td>{{$no}}</td>
             <td>{{$company->name}}</td>
             <td>{{$company->email}}</td>
+            <td>{{$company->address}}</td>
+            <td>
+              <a class="btn btn-labeled bg-primary editbtn text-white" href="/dashboard/companies/{{$company->id}}/edit">Edit</a>
+
+              <form method="POST" action="/dashboard/companies/{{ $company->id }}" >
+                @csrf
+                @method('DELETE')
+                <div class="control">
+                <button type="submit" class="btn btn-danger ms-2" onClick="return confirm('Delete this company?')">Delete</button>
+              </form>
+            </td>
           </tr>
           @php $no++ @endphp
           @endforeach
