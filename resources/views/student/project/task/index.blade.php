7c21f8869fbfface0d36fa4679582711c0bda849
Farhan <faarhan7@gmail.com>
2023-01-24 22:32:24 +0700

    remove task submission text from student card, student cant search project on the student profile, delete placeholder

--- resources/views/student/project/task/index.blade.php
+++ resources/views/student/project/task/index.blade.php
@@ -85,7 +85,7 @@
             <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 ">
             <div class="px-4 py-2 bg-white rounded-t-lg ">
                 <label for="comment" class="sr-only">Your comment</label>
-                <textarea id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0  focus:outline-none dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." name="message" required></textarea>
+                <textarea id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0  focus:outline-none dark:text-white dark:placeholder-gray-400" name="message" required></textarea>
             </div>
             <div class="flex items-center justify-between px-3 py-2 bg-white ">
                 <div class="flex pl-0 space-x-1 sm:pl-2">
