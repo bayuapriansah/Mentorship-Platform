35af16ac0ecbcfc907f549ddcd2fabb32ac9bacb
Farhan <faarhan7@gmail.com>
2023-01-31 13:38:39 +0700

    wip

--- resources/views/layouts/profile/index.blade.php
+++ resources/views/layouts/profile/index.blade.php
@@ -94,46 +94,49 @@
       <div class="col-start-9 col-span-4 flex relative ">
         @if (!Route::is('student.edit'))
           @include('layouts.profile.sidebar')
+          @else
+          <div class="w-full bg-white absolute -top-5 rounded-xl border border-light-blue p-4">
+            <div class="grid grid-cols-12 gap-2 grid-flow-col">
+              <div class="col-span-2">
+                <button type="button" class="relative inline-flex items-center text-sm font-medium text-center text-light-blue rounded-lg hover:text-dark-blue focus:ring-4 focus:outline-none focus:ring-blue-300" alt="notification_bel">
+                  <svg class="w-6 h-6"  aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" stroke-linecap="round" stroke-linejoin="round"></path>
+                  </svg>
+                  <span class="sr-only">Notifications Bell</span>
+                  {{-- <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-dark-blue border-2 border-white rounded-full -top-2 -right-3">{{ $newMessage->count() }}</div> --}}
+                  </button>
+              </div>
+              <div class="col-span-2">
+                <button type="button" data-modal-target="message-modal" data-modal-toggle="message-modal" class="relative inline-flex items-center text-sm font-medium text-center text-light-blue rounded-lg hover:text-dark-blue focus:ring-4 focus:outline-none focus:ring-blue-300" alt="message">
+                  <svg class="w-6 h-6" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" stroke-linecap="round" stroke-linejoin="round"></path>
+                  </svg>
+                  <span class="sr-only">Notifications Message</span>
+                  <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-dark-blue hover:bg-dark-blue border-2 border-white rounded-full -top-2 -right-3">{{ $newMessage->count() }}</div>
+                </button>
+              </div>
+              <div class="col-span-2">
+                <a href="/profile/{{Auth::guard('student')->user()->id}}/edit" type="button" class="relative inline-flex items-center text-sm font-medium text-center text-light-blue rounded-lg hover:text-dark-blue focus:ring-4 focus:outline-none focus:ring-blue-300">
+                  <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
+                  </svg>
+                  <span class="sr-only">Profile edit</span>
+                </a>
+              </div>
+              <div class="col-end-13">
+                <form class="inline" method="post" action="{{ route('logout') }}">
+                  @csrf
+                  <button type="submit" class="relative inline-flex items-center text-sm font-medium text-center text-light-blue rounded-lg hover:text-dark-blue focus:ring-4 focus:outline-none focus:ring-blue-300" alt="Logout">
+                  <svg class="w-6 h-6" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" stroke-linecap="round" stroke-linejoin="round"></path>
+                  </svg>
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
         @endif
-        <div class="w-full bg-white absolute -top-5 rounded-xl border border-light-blue p-4">
-          <div class="grid grid-cols-12 gap-2 grid-flow-col">
-            <div class="col-span-2">
-              <button type="button" class="relative inline-flex items-center text-sm font-medium text-center text-light-blue rounded-lg hover:text-dark-blue focus:ring-4 focus:outline-none focus:ring-blue-300" alt="notification_bel">
-                <svg class="w-6 h-6"  aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" stroke-linecap="round" stroke-linejoin="round"></path>
-                </svg>
-                <span class="sr-only">Notifications Bell</span>
-                {{-- <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-dark-blue border-2 border-white rounded-full -top-2 -right-3">{{ $newMessage->count() }}</div> --}}
-                </button>
-            </div>
-            <div class="col-span-2">
-              <button type="button" data-modal-target="message-modal" data-modal-toggle="message-modal" class="relative inline-flex items-center text-sm font-medium text-center text-light-blue rounded-lg hover:text-dark-blue focus:ring-4 focus:outline-none focus:ring-blue-300" alt="message">
-              <svg class="w-6 h-6" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" stroke-linecap="round" stroke-linejoin="round"></path>
-              </svg>
-              <span class="sr-only">Notifications Message</span>
-              <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-dark-blue hover:bg-dark-blue border-2 border-white rounded-full -top-2 -right-3">{{ $newMessage->count() }}</div>
-              </button>
-            </div>
-            <div class="col-span-2">
-              <a href="/profile/{{Auth::guard('student')->user()->id}}/edit" type="button" class="relative inline-flex items-center text-sm font-medium text-center text-light-blue rounded-lg hover:text-dark-blue focus:ring-4 focus:outline-none focus:ring-blue-300">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
-                </svg>
-                <span class="sr-only">Profile edit</span>
-              </a>
-            </div>
-            <div class="col-end-13">
-              <form class="inline" method="post" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit" class="relative inline-flex items-center text-sm font-medium text-center text-light-blue rounded-lg hover:text-dark-blue focus:ring-4 focus:outline-none focus:ring-blue-300" alt="Logout">
-                <svg class="w-6 h-6" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" stroke-linecap="round" stroke-linejoin="round"></path>
-                </svg>
-                </button>
-              </form>
-            </div>
-        </div>
+        
       </div>
     </nav>
   </div>
--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -21,8 +21,101 @@
               </p>
           @enderror
         </div>
+        <div class="flex justify-between mt-4">
+          <input class="border border-light-blue rounded-lg w-64 h-11 py-2 px-4 invalid:text-lightest-grey leading-tight mr-5 focus:outline-none" id="dob" type="date" placeholder="Date of birth" value="{{$student->date_of_birth}}" name="date_of_birth" required><br>
+          @error('date_of_birth')
+              <p class="text-red-600 text-sm mt-1">
+                {{$message}}
+              </p>
+          @enderror
+          
+          <select id="sex" class="border border-light-blue rounded-lg w-64 h-11 py-2 px-4 invalid:text-lightest-grey leading-tight focus:outline-none" name="sex" required>
+            <option value="" class="" id="emptySex" hidden>Sex *</option>
+            <option value="male" {{$student->sex == 'male' ? 'selected' : ''}}>Male</option>
+            <option value="female" {{$student->sex == 'female' ? 'selected' : ''}}>Female</option>
+          </select><br>
+          @error('sex')
+              <p class="text-red-600 text-sm mt-1">
+                {{$message}}
+              </p>
+          @enderror
+        </div>
+        
+        <select id="inputInstitution" class="text w-full border border-light-blue rounded-lg mt-4 h-11 py-2 px-4 leading-tight bg-gray-300 invalid:text-black cursor-not-allowed focus:outline-none" name="institution" disabled>
+          <option value="" hidden>{{$student->institution->name}}</option>
+        </select><br>
+        @error('institution')
+            <p class="text-red-600 text-sm mt-1">
+              {{$message}}
+            </p>
+        @enderror 
+
+        <div class="flex justify-between mt-4">
+          <input class=" border border-light-blue rounded-lg w-1/2 h-11 py-2 px-4 text-lightest-grey::placeholder leading-tight mr-5 bg-gray-300 cursor-not-allowed focus:outline-none" id="ForCountry" type="text" value="{{$student->country}}" placeholder="Country *" name="country" readonly required>
+          <br>
+          @error('country')
+              <p class="text-red-600 text-sm mt-1">
+                {{$message}}
+              </p>
+          @enderror
+
+          <input class=" border border-light-blue rounded-lg w-1/2 h-11 py-2 px-4 text-lightest-grey::placeholder leading-tight bg-gray-300 cursor-not-allowed focus:outline-none" id="ForState" type="text" value="{{$student->state}}" placeholder="State *" name="state" readonly required>
+          <br>
+          @error('state')
+              <p class="text-red-600 text-sm mt-1">
+                {{$message}}
+              </p>
+          @enderror
+        </div>
+
+        @php
+          $study_programs = ['Artificial Intelligence and Machine Learning', 'Computer Science','Computing Systems', 'Software Engineering'];
+        @endphp
+        <select id="inputStudy" class="text w-full border border-light-blue rounded-lg mt-4 h-11 py-2 px-4 leading-tight invalid:text-lightest-grey focus:outline-none " name="study_program" required>
+          <option value="" hidden>Study Name</option>
+          @foreach($study_programs as $study_program)
+          <option value="{{$study_program}}" {{$student->study_program == $study_program ? 'selected':''}}>{{$study_program}}</option>
+          @endforeach
+          <option value="other">Other</option>
+        </select><br>
+        @error('study_program')
+            <p class="text-red-600 text-sm mt-1">
+              {{$message}}
+            </p>
+        @enderror 
+
       </div>
     </div>
   </div>
 </div>
+@endsection
+@section('more-js')
+<script>
+  $(document).ready(function () {
+      $('#inputInstitution').on('change', function () {
+          var institutionVal = this.value;
+          var base_url = window.location.origin;
+          $.ajax({
+              url: base_url+"/api/institution/"+institutionVal,
+              contentType: "application/json",
+              dataType: 'json',
+              success: function (result) {
+                // console.log(institutionVal);
+                $('#ForCountry').val(result.countries);
+                $('#ForState').val(result.states);
+              }
+          });
+      });
+
+      $('#study_program_form').hide();
+      $("#inputStudy").change(function(){
+        var values = $("#inputStudy option:selected").val();
+        if(values=='other'){
+          $('#study_program_form').show();
+        }else{
+          $('#study_program_form').hide();
+        }
+      });
+  });
+</script>
 @endsection
