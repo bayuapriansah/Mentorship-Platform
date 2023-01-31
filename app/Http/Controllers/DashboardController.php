510ab7bc1dad0b76f641d82601cac5517de35453
Bayu Apriansah <bayuapriansah10@gmail.com>
2023-01-31 16:52:42 +0700

    give some card in dashboard admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use App\Models\Student;
+use App\Models\Mentor;
+use App\Models\EnrolledProject;
+use App\Models\Company;
+
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-      return view('dashboard.index');
+      $students   = Student::get()->count();
+      $mentors    = Mentor::get()->count();
+      $eProjects  = EnrolledProject::get()->count();
+      $companies  = Company::get()->count();
+      return view('dashboard.index', compact('students','mentors','eProjects','companies'));
     }
 
     public function indexCompany()
