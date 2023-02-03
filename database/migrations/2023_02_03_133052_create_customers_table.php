85ff46d08cbd423bfd446b3573f16085da534d09
Bayu Apriansah <bayuapriansah10@gmail.com>
2023-02-03 15:22:52 +0700

    make new migration for customer and change company_id into institution_id in mentors table

--- database/migrations/2023_02_03_133052_create_customers_table.php
+++ database/migrations/2023_02_03_133052_create_customers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('customers', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
