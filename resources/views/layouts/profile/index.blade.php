2a71861cf6cdb3b0a77bd6df3352268f8b0c8efe
Bayu Apriansah <bayuapriansah10@gmail.com>
2023-03-01 16:44:39 +0700

    support button hover show tooltip coming soon v2

--- resources/views/layouts/profile/index.blade.php
+++ resources/views/layouts/profile/index.blade.php
@@ -133,7 +133,7 @@
         <div id="tooltip-hover" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
           Coming Soon
           <div class="tooltip-arrow" data-popper-arrow></div>
-      </div>        
+        </div>        
       </ul>
 
       <div class="col-start-9 col-span-4 flex relative ">
