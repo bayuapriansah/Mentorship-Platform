7f5d103e4dd1314c8203395b2f466cb09bd3bbbc
Farhan <faarhan7@gmail.com>
2023-03-11 13:43:07 +0700

    admin manage certificate

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite('resources/css/app.css')
+
+</head>
+<body>
+  <div class="w-full bg-profile-grey min-h-screen">
+    <nav class="max-w-[1366px] mx-auto px-16 py-4 grid grid-cols-12 gap-14 grid-flow-col items-center">
+      <a href="/" class="col-span-2">
+        <img src="{{asset('assets/img/Intel-logo-2022.png')}}" class="" alt="">
+      </a>
+    </nav>
+    <div class="flex justify-center">
+      <img src="{{url('/assets/img/error_page/400.png')}}" alt="Image"/>
+    </div>
+  </div>
+</body>
+</html>
