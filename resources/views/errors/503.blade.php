f1842f0dfd95d98a49e8d91072d9dfe47eeb094c
Farhan <faarhan7@gmail.com>
2023-03-11 14:08:42 +0700

    503 page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,18 +1,38 @@
-<!doctype html>
-<title>Site Maintenance</title>
-<style>
-  body { text-align: center; padding: 150px; }
-  h1 { font-size: 50px; }
-  body { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-  a { color: #dc8100; text-decoration: none; }
-  a:hover { color: #333; text-decoration: none; }
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite('resources/css/app.css')
 
-<article>
-    <h1>We&rsquo;ll be back soon!</h1>
-    <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p>&mdash; The Team</p>
+</head>
+<body class="bg-profile-grey min-h-screen ">
+  <div class="flex justify-center">
+    <div class="w-4/5 " style="padding: 120px 0;">
+      <div class="flex justify-between">
+        <div class="flex-col my-auto w-1/2 space-y-7">
+          <img src="{{asset('assets/img/Intel-logo-2022.png')}}" class="mb-10" alt="">
+          <h1 class="text-dark-blue my-8 text-3xl">Website is under Maintenance</h1>
+          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, facere. Quos illum nisi quaerat tempora eos accusantium atque, fugiat voluptatibus, asperiores saepe impedit itaque perferendis maxime vero laboriosam numquam. Quidem?</span>
+          <h1 class="text-light-brown text-2xl font-medium space-y-2">
+            Scheduled maintenance <br>
+            <span class="text-black text-2xl">8th March 6:30am - 12:30pm IST.</span>
+          </h1>
+        
+        </div>
+        <div class="w-1/2">
+          <img src="{{url('/assets/img/error_page/under_construction.png')}}" class="!w-[513px] !h-[379px]" style="width: 513px; height: 379px;" alt="Image"/>
+        </div>
+      </div>
     </div>
-</article>
+  </div>
+  <div class="relative">
+    <div class="w-full bg-dark-blue text-white bottom-0">
+      <div class="px-11 pt-12 pb-3">
+        <p class="border-t text-xs">© 2023 Intel Simulated Internships. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
