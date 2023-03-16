2125eae1556a2b78c733eee3c8e18c426caf717a
Bayu Apriansah <bayuapriansah10@gmail.com>
2023-03-16 09:00:58 +0700

    make some changes in information on error page maintenance mode

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -14,11 +14,13 @@
         <div class="flex-col my-auto w-1/2 space-y-7">
           <img src="{{asset('assets/img/Intel-logo-2022.png')}}" class="mb-10" alt="">
           <h1 class="text-dark-blue my-8 text-3xl">Website is under Maintenance</h1>
-          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, facere. Quos illum nisi quaerat tempora eos accusantium atque, fugiat voluptatibus, asperiores saepe impedit itaque perferendis maxime vero laboriosam numquam. Quidem?</span>
-          <h1 class="text-light-brown text-2xl font-medium space-y-2">
+          <span>Thank you for visiting our website. We're currently making some updates to provide you with an even better experience. We're sorry for any inconvenience this may cause and we appreciate your patience as we work to improve our site.</span>
+          <span>We're doing everything we can to minimize the downtime and ensure that our website is back up and running as soon as possible.</span>
+          <span>Thank you for your understanding and support. We look forward to seeing you again soon.</span>
+          {{-- <h1 class="text-light-brown text-2xl font-medium space-y-2">
             Scheduled maintenance <br>
             <span class="text-black text-2xl">8th March 6:30am - 12:30pm IST.</span>
-          </h1>
+          </h1> --}}
         
         </div>
         <div class="w-1/2">
