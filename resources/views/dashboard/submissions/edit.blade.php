704cbd7b76804baa2a644d73705d39caa09e8a23
bayuapriiansah google cloud backup server <bayuapriansah10@gmail.com>
2023-06-06 03:14:17 +0000

    fix copywriting for word cancel

--- resources/views/dashboard/submissions/edit.blade.php
+++ resources/views/dashboard/submissions/edit.blade.php
@@ -89,7 +89,7 @@
                     Confirm
                   </button>
                   <button type="button" data-modal-hide="popup-modal" type="button" class="bg-[#B52809] w-1/2 rounded-lg text-white py-2">
-                    cancel
+                    Cancel
                   </button>
                 </div>
                 {{-- <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
