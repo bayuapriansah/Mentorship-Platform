a7e6cfce2cec601a1e503f3bc817b3835c4d6b9b
unknown <bayuapriansah10@gmail.com>
2023-09-21 17:07:25 +0700

    add : testimonials

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,21 @@
 // import './bootstrap';
 import 'flowbite';
-// Initialization for ES Users
-// import { Rating, initTE } from "tw-elements";
-// initTE({ Rating }, true ); // set second parameter to true if you want to use a debugger
-
+require('./swiperInit');
 document.addEventListener("DOMContentLoaded", function() {
     const textarea = document.getElementById('feedback');
     const counter = document.getElementById('counter');
 
     if (textarea && counter) {
-        // console.log("Textarea and counter found", textarea, counter);  // Debug line
         counter.textContent = `${textarea.value.length}/1000`;
 
         textarea.addEventListener('input', function() {
-            // console.log("Input event triggered");  // Debug line
             counter.textContent = `${this.value.length}/1000`;
 
             if (this.value.length > 1000) {
                 this.value = this.value.substring(0, 1000);
             }
         });
-    } else {
-        // console.log("Textarea or counter not found");  // Debug line
     }
-    
 });
 //     const chatBox = document.getElementById('chat-box');
 //     const userInput = document.getElementById('user-input');
@@ -81,4 +73,3 @@ document.addEventListener("DOMContentLoaded", function() {
 //             });
 //         }
 //     });
-// require('./swiperInit');
