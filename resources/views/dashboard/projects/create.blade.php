686086d21b8c99e3b13dccd7bdfed228bb13577d
Sadam <sadam21x@gmail.com>
2024-01-14 18:27:44 +0700

    refactor(admin): small adjustment to create project page

--- resources/views/dashboard/projects/create.blade.php
+++ resources/views/dashboard/projects/create.blade.php
@@ -5,7 +5,7 @@
     <h1 class="text-dark-blue font-medium text-[1.375rem]">
         {{ $partner->name }}
         <span class="mx-3">></span>
-        Projects
+        Add Project
     </h1>
 
     <a href="{{ $backUrl }}" class="flex items-center gap-3 text-xl">
@@ -53,10 +53,10 @@
         <div class="col-span-4">
             <select id="inputperiod" name="period" class="w-full border border-grey rounded-lg h-11 py-2 px-4 text-lightest-grey::placeholder leading-tight invalid:text-lightest-grey focus:outline-none">
                 <option value="" hidden>Project Duration *</option>
-                <option value="1" {{old('period') == '1' ? 'selected': ''}}>A Week</option>
-                <option value="1" {{old('period') == '1' ? 'selected': ''}}>1 Month</option>
-                <option value="2" {{old('period') == '2' ? 'selected': ''}}>2 Month(s)</option>
-                <option value="3" {{old('period') == '3' ? 'selected': ''}}>3 Month(s)</option>
+                <option value="1" {{ old('period') == '1' ? 'selected': '' }}>A Week</option>
+                <option value="1" {{ old('period') == '1' ? 'selected': '' }}>1 Month</option>
+                <option value="2" {{ old('period') == '2' ? 'selected': '' }}>2 Month(s)</option>
+                <option value="3" {{ old('period') == '3' ? 'selected': '' }}>3 Month(s)</option>
             </select>
 
             @error('period')
@@ -69,8 +69,8 @@
         <div class="col-span-4">
             <select id="inputtype" name="type" class="w-full border border-grey rounded-lg h-11 py-2 px-4 text-lightest-grey::placeholder leading-tight invalid:text-lightest-grey focus:outline-none">
                 <option value="" hidden>Project Type *</option>
-                <option value="weekly">Weekly</option>
-                <option value="monthly">Monthly</option>
+                <option value="weekly" {{ old('type') == 'weekly' ? 'selected' : '' }}>Weekly</option>
+                <option value="monthly" {{ old('type') == 'monthly' ? 'selected' : '' }}>Monthly</option>
             </select>
 
             @error('type')
@@ -79,7 +79,6 @@
                 </p>
             @enderror
         </div>
-
     </div>
 
     <div class="mt-5">
@@ -163,16 +162,16 @@
     <div class="mt-7">
         <div class="flex justify-between items-center gap-6">
             <h3 class="text-dark-blue font-medium text-xl">
-                Add Datasets <span class="text-red-600">*</span>
+                Datasets <span class="text-red-600">*</span>
             </h3>
 
             <button id="add-dataset-btn" type="button" class="flex items-center gap-3 text-xl">
-                <i class="fa-solid fa-circle-plus mt-1 text-primary"></i>
-                Add
+                <i class="fas fa-circle-plus mt-1 text-primary"></i>
+                Add Dataset
             </button>
         </div>
 
-        <div id="dataset-fields-container">
+        <div id="dataset-fields-container" class="mt-4">
             @php
                 $datasets = old('dataset', isset($project) ? $project->dataset : ['']);
                 $datasetCount = count($datasets);
@@ -182,7 +181,7 @@
                 <div class="flex items-center mt-2 relative">
                     <input type="text" class="dataset-input border border-grey rounded-lg w-full h-11 py-2 pl-4 pr-10 text-lightest-grey::placeholder leading-tight focus:outline-none" placeholder="Add Data set URLs separated by semi-colon" name="dataset[]" value="{{ $datasetUrl }}" required>
                     @if ($index > 0)
-                        <button type="button" class="remove-dataset-btn absolute right-2 top-1/2 transform -translate-y-1/2" onclick="removeDatasetInputField(this)"><i class="fa-solid fa-circle-minus text-red-600"></i></button>
+                        <button type="button" class="remove-dataset-btn absolute right-2 top-1/2 transform -translate-y-1/2" onclick="removeDatasetInputField(this)"><i class="fas fa-circle-minus text-red-600"></i></button>
                     @endif
                 </div>
             @endforeach
@@ -202,7 +201,7 @@
             </h3>
 
             <div class="flex items-center gap-3 text-xl text-dark-blue">
-                <i class="fa-solid fa-circle-plus mt-1 text-primary"></i>
+                <i class="fas fa-circle-plus mt-1 text-primary"></i>
                 <input type="submit" class="cursor-pointer" name="addInjectionCard" value="Add Task">
             </div>
         </div>
@@ -272,7 +271,7 @@
                     const removeButton = document.createElement('button');
                     removeButton.type = 'button';
                     removeButton.className = 'remove-dataset-btn absolute right-2 top-1/2 transform -translate-y-1/2';
-                    removeButton.innerHTML = '<i class="fa-solid fa-circle-minus text-red-600"></i>';
+                    removeButton.innerHTML = '<i class="fas fa-circle-minus text-red-600"></i>';
                     removeButton.onclick = function () {
                         inputWrapper.remove();
                     };
