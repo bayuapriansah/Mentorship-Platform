62cf27dfc8fea69e7e061a2a66f54061983a233f
cecep31 <cecepjanuardi31@gmail.com>
2024-03-25 16:50:38 +0700

    handle deleted user

--- resources/views/components/chat/received.blade.php
+++ resources/views/components/chat/received.blade.php
@@ -12,12 +12,23 @@
             $user = $chat->student;
         @endphp
     @endif
-    <img src="{{ $user->profile_picture ? '/storage/' . $user->profile_picture : '/assets/img/profile-placeholder.png' }}"
-        alt="Other User Avatar" class="w-8 h-8 rounded-full ml-3" />
-    <div class="ml-3 bg-gray-100 p-3 rounded-lg shadow-md">
-        <div class="text-xs ">
-            {{ $user->first_name }} {{ $user->last_name }} <span class="capitalize">({{ $chat->sender_type }})</span>
+
+    @if ($user)
+        <img src="{{ $user->profile_picture ? '/storage/' . $user->profile_picture : '/assets/img/profile-placeholder.png' }}"
+            alt="Other User Avatar" class="w-8 h-8 rounded-full ml-3" />
+        <div class="ml-3 bg-gray-100 p-3 rounded-lg shadow-md">
+            <div class="text-xs ">
+                {{ $user->first_name }} {{ $user->last_name }} <span class="capitalize">({{ $chat->sender_type }})</span>
+            </div>
+            <p class="text-sm text-gray-800">{{ $chat->message }}</p>
         </div>
-        <p class="text-sm text-gray-800">{{ $chat->message }}</p>
-    </div>
+    @else
+        <img src="/assets/img/profile-placeholder.png" alt="Other User Avatar" class="w-8 h-8 rounded-full ml-3" />
+        <div class="ml-3 bg-gray-100 p-3 rounded-lg shadow-md">
+            <div class="text-xs ">
+                Deleted User <span class="capitalize">({{ $chat->sender_type }})</span>
+            </div>
+            <p class="text-sm text-gray-800">{{ $chat->message }}</p>
+        </div>
+    @endif
 </div>
